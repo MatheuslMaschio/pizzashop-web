c4dd05dcd0bc04052b11b99cfc912a66a0059915
Matheus Maschio <matheuslmaschi0@hotmail.com>
2024-11-06 00:11:54 -0300

    iniciando projeto

---
+++ src/pages/auth/SignIn.tsx
@@ -0,0 +1,64 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Helmet } from "react-helmet-async"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
+import { z } from "zod"
+
+const signInForm = z.object({
+    email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>
+
+export function SignIn() {
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>();
+
+    async function handleSignIn(data: SignInForm) {
+        try {
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            toast.success("Login efetuado com sucesso!", {
+                action: {
+                    label: "Reenviar",
+                    onClick: () => handleSignIn(data),
+                }
+            });
+        } catch {
+            toast.error("Erro ao efetuar o login");
+        }
+    }
+
+    return (
+        <>
+            <Helmet title="Login" />
+            <div className="p-8">
+                <div className="w-[350px] flex flex-col justify-center gap-6">
+                    <div className="flex flex-col gap-2 text-center">
+                        <h1 className="text-2xl font-semibold tracking-tight">
+                            Acessar Painel
+                        </h1>
+                        <p className="text-sm text-muted-foreground">
+                            Acompanhe suas vendas pelo painel do parceiro:
+                        </p>
+                    </div>
+                    <form onClick={handleSubmit(handleSignIn)} className="space-y-4">
+                        <div className="space-y-2">
+                            <Label htmlFor="email">Seu e-mail</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                {...register("email")}
+                            />
+
+                        </div>
+                        <Button disabled={isSubmitting} className="w-full" type="submit">
+                            Acessar painel
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/routes.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "./pages/app/Dashboard";
+import { SignIn } from "./pages/auth/SignIn";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />, //uso de layout
+        children: [
+            {path: "/", element: <Dashboard />}, //elemento filho
+        ]
+    },
+
+    {
+        path: "/sign-in",
+        element: <AuthLayout />,
+        children: [
+            {path: "/sign-in", element: <SignIn />},
+        ]
+    }
+
+]);
