553e3e062358426f962e6601658eb3c75767849b
MatheuslMaschio <matheuslmaschi0@hotmail.com>
2024-11-07 10:15:35 -0300

    pagina dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/Dashboard";
+
 import { SignIn } from "./pages/auth/SignIn";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/SignUp";
 import { Orders } from "./pages/app/orders/orders";
+import { Dashboard } from "./pages/app/dashboard/Dashboard";
 
 
 export const router = createBrowserRouter([
