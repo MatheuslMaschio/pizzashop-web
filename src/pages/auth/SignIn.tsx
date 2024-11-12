3f0fb4a91c1675413c77ce75a1a9d44dad0280ea
Matheus Maschio <matheuslmaschi0@hotmail.com>
2024-11-12 11:05:09 -0300

    Autenticação de Usuario

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,6 +1,8 @@
+import { signIn } from "@/api/signIn"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useMutation } from "@tanstack/react-query"
 import { Helmet } from "react-helmet-async"
 import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
@@ -16,9 +18,14 @@ type SignInForm = z.infer<typeof signInForm>
 export function SignIn() {
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>();
 
+    const {mutateAsync: authenticate} = useMutation({
+        mutationFn: signIn, 
+    })
+
+
     async function handleSignIn(data: SignInForm) {
         try {
-            await new Promise(resolve => setTimeout(resolve, 2000));
+            await authenticate({email: data.email});
 
             toast.success("Login efetuado com sucesso!", {
                 action: {
