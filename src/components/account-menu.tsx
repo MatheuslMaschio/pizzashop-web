3a25d6ab257c5f915250ee4b9bfbdc3b3bf1d523
Matheus Maschio <matheuslmaschi0@hotmail.com>
2024-11-12 14:06:45 -0300

    Perfil no cabecalho

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,21 +1,34 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccountMenu() {
+    const {data: profile} = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile
+    });
+
+    const {data: managedRestaurant} = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant
+    });
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" className="flex items-center gap-2 select-none">
-                    Pizza Shop
+                    {managedRestaurant?.name}
                     <ChevronDown className="h-4 w-4" />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel className="flex flex-col">
-                    <span>Matheus Maschio</span>
+                    <span>{profile?.name}</span>
                     <span className="text-sm font-normal text-muted-foreground">
-                        Matheus@email.com.br
+                        {profile?.email}
                     </span>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
@@ -25,7 +38,7 @@ export function AccountMenu() {
                 </DropdownMenuItem>
 
                 <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <LogOut className="w-4 h-4 mr-2" />
+                    <LogOut className="brw-4 h-4 mr-2" />
                     <span>Sair</span>
                 </DropdownMenuItem>
             </DropdownMenuContent>
