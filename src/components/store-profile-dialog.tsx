7971b81fdc082ddeb62a30d7bff71299d49f47f7
Matheus Maschio <matheuslmaschi0@hotmail.com>
2024-11-13 09:12:16 -0300

    Interface otimista

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -13,7 +13,7 @@ import { toast } from "sonner";
 
 const storeProfileSchema = z.object({
     name: z.string().min(1),
-    description: z.string(),
+    description: z.string().nullable(),
 });
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>;
@@ -21,7 +21,7 @@ type StoreProfileSchema = z.infer<typeof storeProfileSchema>;
 export function StoreProfileDialog() {
     const queryClient = useQueryClient();
 
-    const { data: managedRestaurant} = useQuery({
+    const { data: managedRestaurant } = useQuery({
         queryKey: ['managed-restaurant'],
         queryFn: getManagedRestaurant,
         staleTime: Infinity
@@ -30,38 +30,51 @@ export function StoreProfileDialog() {
     const {
         register,
         handleSubmit,
-        formState: {isSubmitting}
+        formState: { isSubmitting }
     } = useForm<StoreProfileSchema>({
         resolver: zodResolver(storeProfileSchema),
         values: {
-            name: managedRestaurant?.name ?? '', 
+            name: managedRestaurant?.name ?? '',
             description: managedRestaurant?.description ?? ''
         }
     });
 
-    const {mutateAsync: updateProfileFn} = useMutation({
-        mutationFn: updateProfile,
-        onSuccess(_,{name, description}){
-            const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant']);
+    function updateManagedRestaurantCached({ name, description }: StoreProfileSchema) {
+        const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant']);
 
-            if(cached){
-                queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
-                    ...cached, 
-                    name, 
-                    description,
-                });
+        if (cached) {
+            queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
+                ...cached,
+                name,
+                description,
+            });
+        }
+
+        return {cached};
+    }
+
+    const { mutateAsync: updateProfileFn } = useMutation({
+        mutationFn: updateProfile,
+        onMutate({ name, description }) {
+          const {cached} =  updateManagedRestaurantCached({ name, description });
+
+          return {previousProfile: cached};
+        },
+        onError(_, __,context) {
+            if(context?.previousProfile){
+                updateManagedRestaurantCached(context.previousProfile);
             }
         }
     })
 
-    async function handleUpdateProfile(data: StoreProfileSchema){
-        try{
+    async function handleUpdateProfile(data: StoreProfileSchema) {
+        try {
             await updateProfileFn({
                 name: data.name,
                 description: data.description
             });
             toast.success('Perfil atualizado com sucesso!');
-        }catch{
+        } catch {
             toast.error('Erro ao atualizar perfil, tente novamente!');
         }
     }
