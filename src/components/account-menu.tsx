04a23d29634ba8251f560327c79717f0341dae10
Matheus Maschio <matheuslmaschi0@hotmail.com>
2024-11-13 09:22:34 -0300

    logout do usuario

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,14 +1,17 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Skeleton } from "./ui/skeleton";
 import { Dialog, DialogTrigger } from "./ui/dialog";
 import { StoreProfileDialog } from "./store-profile-dialog";
+import { signOut } from "@/api/signOut";
+import { useNavigate } from "react-router-dom";
 
 export function AccountMenu() {
+    const  navigate  = useNavigate();
     const { data: profile, isLoading: isLoadingProfile } = useQuery({
         queryKey: ['profile'],
         queryFn: getProfile
@@ -19,6 +22,13 @@ export function AccountMenu() {
         queryFn: getManagedRestaurant,
         staleTime: Infinity,
     });
+
+    const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+        mutationFn: signOut,
+        onSuccess: () => {
+            navigate('/sign-in', { replace: true });
+        }
+    })
 
     return (
         <Dialog>
@@ -40,8 +50,8 @@ export function AccountMenu() {
                     <DropdownMenuLabel className="flex flex-col">
                         {isLoadingProfile ? (
                             <div className="space-y-1.5">
-                                <Skeleton className="h-4 w-32"/>
-                                <Skeleton className="h-3 w-24"/>
+                                <Skeleton className="h-4 w-32" />
+                                <Skeleton className="h-3 w-24" />
                             </div>
                         ) : (
                             <>
@@ -59,9 +69,15 @@ export function AccountMenu() {
                             <span>Perfil da loja</span>
                         </DropdownMenuItem>
                     </DialogTrigger>
-                    <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                        <LogOut className="brw-4 h-4 mr-2" />
-                        <span>Sair</span>
+                    <DropdownMenuItem
+                        className="text-rose-500 dark:text-rose-400"
+                        asChild
+                        disabled={isSigningOut}
+                    > 
+                        <button className="w-full" onClick={() => signOutFn()}>
+                            <LogOut className="brw-4 h-4 mr-2" />
+                            <span>Sair</span>
+                        </button>
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
