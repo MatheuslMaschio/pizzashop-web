a18c2f33f696ac480f5b70a24386f7fa72c24fe6
Matheus Maschio <matheuslmaschi0@hotmail.com>
2024-12-11 16:49:57 -0300

    Erros e página 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,31 +1,49 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom'
 
-import { SignIn } from "./pages/auth/SignIn";
-import { AppLayout } from "./pages/_layouts/app";
-import { AuthLayout } from "./pages/_layouts/auth";
-import { SignUp } from "./pages/auth/SignUp";
-import { Dashboard } from "./pages/app/dashboard/dashboard";
-import { Orders } from "./pages/app/dashboard/orders/orders";
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { SignUp } from './pages/auth/SignUp'
+import { SignIn } from './pages/auth/SignIn'
+import { Orders } from './pages/app/dashboard/orders/orders'
+import { Error } from './pages/error'
+import { NotFound } from './pages/404'
 
 
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <AppLayout />, //uso de layout
-        children: [
-            {path: "/", element: <Dashboard />}, //elemento filho
-            {path: "/orders", element: <Orders />},
-        ]
-    },
-
-    {
-        path: "/",
-        element: <AuthLayout />,
-        children: [
-            {path: "/sign-in", element: <SignIn />},
-            {path: "/sign-up", element: <SignUp />},
-        ]
-    }
-
-]);
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+])
